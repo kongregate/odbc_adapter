7cc128a864e0cb8e78acae5f994210bfd8c0e441
Max Murphy <max@kongregate.com>
2019-04-04 16:11:44 -0700

    feat: make compatible with rails 5.2

--- lib/odbc_adapter/adapters/mysql_odbc_adapter.rb
+++ lib/odbc_adapter/adapters/mysql_odbc_adapter.rb
@@ -6,7 +6,7 @@ module ODBCAdapter
       PRIMARY_KEY = 'INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY'.freeze
 
       class BindSubstitution < Arel::Visitors::MySQL
-        include Arel::Visitors::Visitor
+        include Arel::Visitors
       end
 
       def arel_visitor
